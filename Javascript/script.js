7df20464ad2b41b0a916209003bf05499a568883
mansoor <mansoorshaik7b@gmail.com>
2024-12-18 16:11:12 +0530

    javascript

---
+++ Javascript/script.js
@@ -0,0 +1,22 @@
+document.getElementById('addTaskButton').addEventListener('click', addTask);
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = taskInput.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', function () {
+            taskList.removeChild(li);
+        });
+
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+
+        taskInput.value = '';
+    }
+}
